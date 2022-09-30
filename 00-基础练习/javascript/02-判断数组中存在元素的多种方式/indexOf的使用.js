759700fa8453852f4efe4d2c495b2395d9c183fb
txb <1013984359@qq.com>
2022-09-30 14:08:27 +0800

    判断数组中存在的元素的多种方式

--- 00-基础练习/javascript/02-判断数组中存在元素的多种方式/indexOf的使用.js
+++ 00-基础练习/javascript/02-判断数组中存在元素的多种方式/indexOf的使用.js
@@ -2,12 +2,12 @@ const useIndexOf=(arr) =>{
     //indexOf的使用，indexOf分为单参数和多参数，不论那种参数类型，indexOf都是顺序遍历数组
 // 单参数的indexOf的参数值是数组中的元素，只能传入基本类型，从起始位0开始索引，
 // 如果数组中不存在传入参数的值，返回-1，存在返回索引值。
-    number=arr.indexOf(false);
-    console.log('数组长度15,number=arr.indexOf(false);返回的索引值是: '+number)
     let number = arr.indexOf(1); //索引值为1
     console.log('数组长度15,number=arr.indexOf(1);返回的索引值是: '+number);
     number=arr.indexOf(5);
     console.log('数组长度15,number=arr.indexOf(5);返回的索引值是: '+number);
+    number=arr.indexOf(false);
+    console.log('数组长度15,number=arr.indexOf(false);返回的索引值是: '+number)
 //indexOf 的第二个参数fromIndex（起始索引值），
 //证明indexOf是顺序遍历arr数组
     number=arr.indexOf(6,14);
